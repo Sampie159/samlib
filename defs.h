4edb366071c23c0831e2e0eb26f1ba548b7882d9
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2024-01-10 21:59:14 -0300

    Added a couple useful macros

--- defs.h
+++ defs.h
@@ -7,6 +7,10 @@
 #ifndef __cplusplus
 #include <stdbool.h>
 
+#define len(arr) sizeof(arr)/sizeof(arr[0])
+#define for_each(arr) for (u64 i = 0; i < len(arr); i++)
+#define for_eachr(arr) for (i64 i = len(arr) - 1; i >= 0; i--)
+
 typedef char * string;
 #endif
 
