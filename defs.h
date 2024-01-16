ad69f0daa7041e9f5fd23eba38481c813fc9a5a8
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2024-01-16 18:00:48 -0300

    added my own boolean, added new "char" types

--- defs.h
+++ defs.h
@@ -5,26 +5,30 @@
 #define local_persist static
 
 #ifndef __cplusplus
-#include <stdbool.h>
-
 #define len(arr) sizeof(arr)/sizeof(arr[0])
 #define for_each(arr) for (u64 i = 0; i < len(arr); i++)
 #define for_eachr(arr) for (i64 i = len(arr) - 1; i >= 0; i--)
+#define true 1
+#define false 0
 
+typedef char bool;
 typedef char * string;
+
 #else
+
 #define restrict __restrict
+
 #endif
 
-typedef char u8; // I don't know if char will actually be unsigned, but for the sake of clarity I chose `u8`
-
+typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long u64;
 
-typedef short i16;
-typedef int i32;
-typedef long i64;
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long i64;
 
 typedef float f32;
 typedef double f64;
