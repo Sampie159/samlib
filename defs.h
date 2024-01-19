48b9c2dc6630d0980e0a0bd2d36c73b8dcc8fe2e
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2024-01-19 05:38:36 -0300

    added definitions for bitflags

--- defs.h
+++ defs.h
@@ -1,18 +1,24 @@
 #pragma once
 
-#define global static
-#define internal static
+#define global        static
+#define internal      static
 #define local_persist static
+
+//Bitflags definitions
+#define ALL8  0xff
+#define ALL16 0xffff
+#define ALL32 0xffffffff
+#define ALL64 0xffffffffffffffff
 
 #ifndef __cplusplus
 #define len(arr) sizeof(arr)/sizeof(arr[0])
 #define for_each(arr) for (u64 i = 0; i < len(arr); i++)
 #define for_eachr(arr) for (i64 i = len(arr) - 1; i >= 0; i--)
-#define true 1
+#define true  1
 #define false 0
 
-typedef char bool;
 typedef char * string;
+typedef char   bool;
 
 #else
 
@@ -20,15 +26,15 @@ typedef char * string;
 
 #endif
 
-typedef unsigned char u8;
+typedef unsigned char  u8;
 typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long u64;
+typedef unsigned int   u32;
+typedef unsigned long  u64;
 
-typedef signed char i8;
+typedef signed char  i8;
 typedef signed short i16;
-typedef signed int i32;
-typedef signed long i64;
+typedef signed int   i32;
+typedef signed long  i64;
 
-typedef float f32;
+typedef float  f32;
 typedef double f64;
