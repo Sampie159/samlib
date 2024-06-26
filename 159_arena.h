4f694ddc53fb50749eafde9cb0af881d7d652e62
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2024-06-25 21:49:26 -0300

    refactor: added clang-format

--- 159_arena.h
+++ 159_arena.h
@@ -19,68 +19,67 @@ typedef enum {
 } Buffer_Type;
 
 typedef struct {
-    void *buffer;
+    void*         buffer;
     unsigned long pos;
     unsigned long size;
 } Stack_Buffer;
 
 typedef struct growing_buffer {
-    void *buffer;
-    struct growing_buffer *next;
-    unsigned long pos;
-    unsigned long size;
+    void*                  buffer;
+    struct growing_buffer* next;
+    unsigned long          pos;
+    unsigned long          size;
 } Growing_Buffer;
 
 typedef struct {
     Buffer_Type buffer_type;
+
     union {
-        Stack_Buffer sb;
+        Stack_Buffer   sb;
         Growing_Buffer gb;
     };
 } Arena;
 
 // Initializes a new `Arena` with the given `buffer` of length `buf_len`.
 // It's size does NOT grow.
-void arena_init_buffer(Arena *a, void *buffer, unsigned long buf_len) {
+void arena_init_buffer(Arena* a, void* buffer, unsigned long buf_len) {
     a->buffer_type = STACK_BUFFER;
-    a->sb.buffer = buffer;
-    a->sb.pos = 0;
-    a->sb.size = buf_len;
+    a->sb.buffer   = buffer;
+    a->sb.pos      = 0;
+    a->sb.size     = buf_len;
 }
 
 // Initializes a new `Arena` that utilizes a Linked List structure.
-void arena_init_growing(Arena *a) {
+void arena_init_growing(Arena* a) {
     a->buffer_type = GROWING_BUFFER;
-    a->gb.buffer = malloc(DEFAULT_GROWING_SIZE);
-    a->gb.pos = 0;
-    a->gb.next = NULL;
-    a->gb.size = DEFAULT_GROWING_SIZE;
+    a->gb.buffer   = malloc(DEFAULT_GROWING_SIZE);
+    a->gb.pos      = 0;
+    a->gb.next     = NULL;
+    a->gb.size     = DEFAULT_GROWING_SIZE;
 }
 
 // Initializes a new `Arena` that utilizes a Linked List structure with size `size`.
-void arena_init_growing2(Arena *a, unsigned long size) {
+void arena_init_growing2(Arena* a, unsigned long size) {
     a->buffer_type = GROWING_BUFFER;
-    a->gb.buffer = malloc(size);
-    a->gb.pos = 0;
-    a->gb.next = NULL;
-    a->gb.size = size;
+    a->gb.buffer   = malloc(size);
+    a->gb.pos      = 0;
+    a->gb.next     = NULL;
+    a->gb.size     = size;
 }
 
 // Frees all the memory allocated by a `Growing_Buffer`, basically a no op for a `Stack_Buffer`.
 // Makes the `Arena` unusable in either case, until another `arena_init`.
-void arena_free(Arena *a) {
+void arena_free(Arena* a) {
     switch (a->buffer_type) {
-    case STACK_BUFFER:
-        a->buffer_type = NONE;
-        break;
+    case STACK_BUFFER: a->buffer_type = NONE; break;
     case GROWING_BUFFER:
         a->buffer_type = NONE;
         {
-            Growing_Buffer *cur = a->gb.next;
+            Growing_Buffer* cur = a->gb.next;
             while (cur) {
                 free(cur->buffer);
-                Growing_Buffer *aux = cur;
-                cur = cur->next;
+                Growing_Buffer* aux = cur;
+                cur                 = cur->next;
                 free(aux);
             }
             free(a->gb.buffer);
@@ -91,73 +90,69 @@ void arena_free(Arena *a) {
 }
 
 // Resets the memory used by the allocator to be reused.
-void arena_reset(Arena *a) {
+void arena_reset(Arena* a) {
     switch (a->buffer_type) {
     case STACK_BUFFER:
         memset(a->sb.buffer, 0, a->sb.size);
         a->sb.pos = 0;
         break;
-    case GROWING_BUFFER:
-        {
-            Growing_Buffer *cur = a->gb.next;
-            while (cur) {
-                free(cur->buffer);
-                Growing_Buffer *aux = cur;
-                cur = cur->next;
-                free(aux);
-            }
-            memset(a->gb.buffer, 0, a->gb.size);
-            a->gb.next = NULL;
-            a->gb.pos = 0;
+    case GROWING_BUFFER: {
+        Growing_Buffer* cur = a->gb.next;
+        while (cur) {
+            free(cur->buffer);
+            Growing_Buffer* aux = cur;
+            cur                 = cur->next;
+            free(aux);
         }
-        break;
+        memset(a->gb.buffer, 0, a->gb.size);
+        a->gb.next = NULL;
+        a->gb.pos  = 0;
+    } break;
     default: break;
     }
 }
 
 // Returns a `NULL` pointer if it's a `Stack_Buffer` and doesn't have enough space
 // or if the `Arena` has been deinitialized.
-void *arena_alloc(Arena *a, unsigned long size) {
-    void *ret;
+void* arena_alloc(Arena* a, unsigned long size) {
+    void* ret;
 
     switch (a->buffer_type) {
     case STACK_BUFFER:
         if (a->sb.pos + size > a->sb.size) return NULL;
-        ret = (char *)a->sb.buffer + a->sb.pos;
+        ret        = (char*)a->sb.buffer + a->sb.pos;
         a->sb.pos += size;
         break;
-    case GROWING_BUFFER:
-        {
-            Growing_Buffer *aux = &a->gb;
-            while (1) {
-                if (aux->pos + size > a->gb.size) {
-                    if (aux->next) {
-                        aux = aux->next;
-                        continue;
-                    }
-                    
-                    const unsigned long size_to_grow = (unsigned long)ceil((double)size / a->gb.size) * a->gb.size;
-
-                    aux->next = (Growing_Buffer *)malloc(sizeof(Growing_Buffer));
+    case GROWING_BUFFER: {
+        Growing_Buffer* aux = &a->gb;
+        while (1) {
+            if (aux->pos + size > a->gb.size) {
+                if (aux->next) {
                     aux = aux->next;
-                    aux->buffer = malloc(size_to_grow);
-                    aux->pos = size;
-                    aux->size = size_to_grow;
-                    aux->next = NULL;
-                    ret = aux->buffer;
-                    break;
+                    continue;
                 }
 
-                ret = (char *)aux->buffer + aux->pos;
-                aux->pos += size;
+                const unsigned long size_to_grow = (unsigned long)ceil((double)size / a->gb.size) * a->gb.size;
+
+                aux->next   = (Growing_Buffer*)malloc(sizeof(Growing_Buffer));
+                aux         = aux->next;
+                aux->buffer = malloc(size_to_grow);
+                aux->pos    = size;
+                aux->size   = size_to_grow;
+                aux->next   = NULL;
+                ret         = aux->buffer;
                 break;
             }
+
+            ret       = (char*)aux->buffer + aux->pos;
+            aux->pos += size;
+            break;
         }
-        break;
+    } break;
     default: ret = NULL;
     }
 
     return ret;
 }
 
-#endif // _159_ARENA_H_
+#endif   // _159_ARENA_H_
--- defs.h
+++ defs.h
@@ -4,56 +4,56 @@
 #include <stdlib.h>
 
 #ifndef NDEBUG
-#define TODO(format, ...) fprintf(stderr, "\033[1m[TODO] \033[0m" format __VA_OPT__(,) __VA_ARGS__)
-#define WARN(format, ...) fprintf(stderr, "\033[1;33m[WARNING] \033[0m" format __VA_OPT__(,) __VA_ARGS__)
-#define ERROR(format, ...) fprintf(stderr, "\033[1;31m[ERROR] \033[0m" format __VA_OPT__(,) __VA_ARGS__)
-#define OK(format, ...) fprintf(stderr, "\033[1;32m[OK] \033[0m" format __VA_OPT__(,) __VA_ARGS__)
+    #define TODO(format, ...)  fprintf(stderr, "\033[1m[TODO] \033[0m" format __VA_OPT__(, ) __VA_ARGS__)
+    #define WARN(format, ...)  fprintf(stderr, "\033[1;33m[WARNING] \033[0m" format __VA_OPT__(, ) __VA_ARGS__)
+    #define ERROR(format, ...) fprintf(stderr, "\033[1;31m[ERROR] \033[0m" format __VA_OPT__(, ) __VA_ARGS__)
+    #define OK(format, ...)    fprintf(stderr, "\033[1;32m[OK] \033[0m" format __VA_OPT__(, ) __VA_ARGS__)
 #else
-#define TODO(format, ...)
-#define WARN(format, ...)
-#define ERROR(format, ...)
-#define OK(format, ...)
+    #define TODO(format, ...)
+    #define WARN(format, ...)
+    #define ERROR(format, ...)
+    #define OK(format, ...)
 #endif
 
 #define UNIMPLEMENTED() fprintf(stderr, "\033[1m[TODO] \033[0m%s is unimplemented!\n", __FUNCTION__)
-#define PANIC(format, ...)                                                          \
-	fprintf(stderr, "\033[1;31m[PANIC] \033[0m" format __VA_OPT__(,) __VA_ARGS__);	\
-	exit(EXIT_FAILURE)
+#define PANIC(format, ...)                                                                                             \
+    fprintf(stderr, "\033[1;31m[PANIC] \033[0m" format __VA_OPT__(, ) __VA_ARGS__);                                    \
+    exit(EXIT_FAILURE)
 
 #define global        static
 #define internal      static
 #define local_persist static
 
-//Bitflags definitions
+// Bitflags definitions
 #define ALL8  0xff
 #define ALL16 0xffff
 #define ALL32 0xffffffff
 #define ALL64 0xffffffffffffffff
 
 #ifdef _MSC_VER
-#define INLINE __forceinline
+    #define INLINE __forceinline
 #else
-#define INLINE __attribute__((always_inline)) inline
+    #define INLINE __attribute__((always_inline)) inline
 #endif
 
 #ifndef __cplusplus
-#define LEN(arr) sizeof(arr)/sizeof(arr[0])
-#define for_each(arr) for (u64 i = 0; i < LEN(arr); i++)
-#define for_eachr(arr) for (i64 i = LEN(arr) - 1; i >= 0; i--)
-#define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
-#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+    #define LEN(arr)       sizeof(arr) / sizeof(arr[0])
+    #define for_each(arr)  for (u64 i = 0; i < LEN(arr); i++)
+    #define for_eachr(arr) for (i64 i = LEN(arr) - 1; i >= 0; i--)
+    #define MIN(X, Y)      ((X) < (Y) ? (X) : (Y))
+    #define MAX(X, Y)      ((X) > (Y) ? (X) : (Y))
 
-#if !(__STDC_VERSION__ > 201710)
-#define true  1
-#define false 0
-typedef char  bool;
-#endif
+    #if !(__STDC_VERSION__ > 201710)
+        #define true  1
+        #define false 0
+typedef char bool;
+    #endif
 
-typedef char * string;
+typedef char* string;
 
 #else
 
-#define restrict __restrict
+    #define restrict __restrict
 
 #endif
 
